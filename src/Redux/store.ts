0d0b23c8f43913de1bd5666551449cebee766f7e
Amal <Your GitHub Email>
2025-11-28 22:45:12 +0200

    Create Add NEw Project Page

---
+++ src/Redux/store.ts
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReduser from "./Slices/userData.js";
+
+export const store = configureStore({
+  reducer: {
+    // create reducer as an object because it may there more than one reducer (slice)
+    user: userReduser, //key: value
+  },
+});
