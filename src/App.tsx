024be944d3de38b43e54d76f1e577bf5f390a41b
Amal <amal.nasr.1421@gmail.com>
2026-03-04 22:19:59 +0200

    Fix import path for ProtectedLoader to remove file extension

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import InviteMembers from "./Pages/Subpages/InviteMembers.js";
 import AddNewEpic from "./Pages/Epics/AddNewEpic.js";
 import Tasks from "./Pages/Tasks/Tasks.js";
 import GetEpics from "./Pages/Epics/GetEpics.js";
-import ProtectedLoader from "./Routes/ProtectedLoader.js";
+import ProtectedLoader from "./Routes/ProtectedLoader";
 
 const routes = createBrowserRouter([
   {
